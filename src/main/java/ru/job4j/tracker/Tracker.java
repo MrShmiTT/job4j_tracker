d20544c4377e083cba965092cd7b4738e02a6277
Alex <alexandr.vatutin@gmail.com>
2021-08-17 17:31:52 +0300

    5. Tracker - хранилище [#396 #190976]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -26,14 +26,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        int count = 0;
-        Item[] findAll = new Item[size];
-        for (int i = 0; i < size; i++) {
-            if (items[i] != null) {
-                findAll[count++] = items[i];
-            }
-        }
-        return Arrays.copyOf(findAll, count);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
