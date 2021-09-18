d0f06e8778b3e5844e37625e4f3153bf17d440f9
Aleksandr Vatutin <shmitt87@mail.ru>
2021-09-18 15:46:35 +0300

    Lesson приведение типов

---
+++ src/main/java/ru/job4j/oop/DogOld.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class DogOld {
+    public static void main(String[] args) {
+        DogOld polkan = new DogOld();
+        DogOld sharik = new DogOld();
+        DogOld zhychka = new DogOld();
+    }
+}
