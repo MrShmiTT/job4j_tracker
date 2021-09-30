f3d52c4a82c27849d3746c3e4904bb6f1df0f964
Aleksandr Vatutin <alexandr.vatutin@gmail.com>
2021-09-30 14:06:40 +0300

    0.4. Пользовательские исключения. [#219367 #209973]

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -5,29 +5,4 @@ public class ElementNotFoundException extends Exception {
     public ElementNotFoundException(String message) {
         super(message);
     }
-
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (value[i] == key) {
-                rsl = i;
-                break;
-            }
-            if (value[i] != key) {
-                throw new ElementNotFoundException("Element not found");
-            }
-        }
-        return rsl;
-    }
-
-    public static void main(String[] args) {
-        String[] str = {"Ebay", "Amazon"};
-        String key = "Ozon";
-
-        try {
-            indexOf(str, key);
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
 }
